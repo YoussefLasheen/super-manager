971967f60fe8a4e78e496f34c01d98c720ca94bb
unknown <ylasheen5@gmail.com>
2019-10-05 08:41:51 +0200

    Navigate to chat_screen

--- lib/screens/dummy_screen.dart
+++ lib/screens/dummy_screen.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:supermanager/screens/chat_screen.dart';
 import '../flurry_navigation.dart';
 
 final Screen dummy_screen = new Screen(
     contentBuilder: (BuildContext context) {
-      return new Scaffold(
-        body:Center(
-        child: new Container(
-          height:100.0,
-          child: new Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: new Column(
-              children: [
-                new Expanded(
-                    child: new Center(
-                        child: new Text("Hello")
-                    )
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-      );
+      return ChatScreen("joe");
     }
 );
