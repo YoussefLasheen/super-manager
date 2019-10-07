59e34dc54aa2a0c48d9567df1e403b8dc3387549
unknown <ylasheen5@gmail.com>
2019-10-07 10:52:00 +0200

    return back to just text for now

--- lib/screens/dummy_screen.dart
+++ lib/screens/dummy_screen.dart
@@ -4,6 +4,6 @@ import '../flurry_navigation.dart';
 
 final Screen dummy_screen = new Screen(
     contentBuilder: (BuildContext context) {
-      return ChatScreen("joe");
+      return Text("H9");
     }
 );
