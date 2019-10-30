ea2be61af7bd5173c2adbcc045b26fce2f10885e
unknown <ylasheen5@gmail.com>
2019-10-30 18:04:58 +0200

    check if null to build system chat

--- lib/chat_widget.dart
+++ lib/chat_widget.dart
@@ -5,7 +5,7 @@ class ChatWidget extends StatelessWidget {
   final Map chat;
   final bool isReceived;
   final bool isSuccessful;
-  const ChatWidget({Key key,@required this.chat,this.isReceived=false, this.isSuccessful=true}) : super(key: key);
+  const ChatWidget({Key key,@required this.chat,this.isReceived=true, this.isSuccessful=true}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,7 +14,7 @@ class ChatWidget extends StatelessWidget {
       children: <Widget>[
         Padding(
           padding:  EdgeInsets.symmetric(vertical: 6.0,horizontal: 12.0),
-          child: isReceived?_buildReceivedMessage(context):_buildSentMessage(context),
+          child: isReceived == null?_buildSystemMessage(context):isReceived?_buildReceivedMessage(context):_buildSentMessage(context),
         ),
       ],
     );
@@ -43,5 +43,19 @@ class ChatWidget extends StatelessWidget {
             child: Text(chat['content'],style: TextStyle(fontSize: 18.0,color: Colors.black),),
       ),
     );
+  }
+  Widget _buildSystemMessage(BuildContext context){
+    return Container(
+                padding: EdgeInsets.only(top:50),
+                alignment: Alignment.center,
+                child: Container(
+                  constraints: BoxConstraints(maxWidth:MediaQuery.of(context).size.width*3/4 ),
+                  padding: EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xfffff5c4),
+                    borderRadius: BorderRadius.circular(15.0),),
+                  child: Text(chat['content'],style: TextStyle(fontSize: 18.0,color: Colors.black54),),
+                  ),
+                );
   }
 }
