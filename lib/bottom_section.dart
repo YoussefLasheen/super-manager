2318783492ab22d59d8dc35dc9dbcf7e7f8a1632
unknown <ylasheen5@gmail.com>
2019-10-30 17:54:29 +0200

    Add the new tasks page in the pageview

--- lib/bottom_section.dart
+++ lib/bottom_section.dart
@@ -3,7 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supermanager/api.dart';
+import 'package:supermanager/widgets/add_task_card.dart';
+import 'package:supermanager/widgets/task_card.dart';
 
+import 'models/task.dart';
 import 'models/user.dart';
 import 'widgets/gesture_card.dart';
 
@@ -113,6 +116,39 @@ class _BottomSectionState extends State<BottomSection> {
               pageSnapping: true,
               scrollDirection: Axis.vertical,
               children: <Widget>[
+                  StreamBuilder<DocumentSnapshot>(
+                    stream: Firestore.instance.collection('users')
+                    .document(user.uid)
+                    .collection('Data')
+                    .document('Data')
+                    .snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) return LinearProgressIndicator();
+                      return PageView.builder(
+                        controller: PageController(initialPage: 1, viewportFraction: 0.8),
+                        itemBuilder: (BuildContext context, int index) {
+                          //String key = snapshot.data['Tasks'].keys.elementAt(index);
+                          var keys = snapshot.data['Tasks'].keys.toList();
+                          if (index == snapshot.data['Tasks'].length)
+                            return AddPageCard(color: Colors.blueGrey,userUID: user.uid,);
+                            var val = snapshot.data['Tasks'][keys[index]];
+                            return TaskCard(
+                            task: Task(val['title'] ,codePoint: val['codePoint'],color: val['color'],dueDate: val['dueDate']?.toDate(),id: keys[index]),
+                            todos: val['todos'],
+                            taskCompletionPercent: val['todos'].length == 0
+                            ? 100
+                            : ((val['todos'].where((user) => user['isCompleted'] == true).length /
+                             val['todos'].length)
+                             *100)
+                             .toInt(),
+                            totalTodos: val['todos'].length,
+                          );
+                          
+                        },
+                        itemCount: snapshot.data['Tasks'].length +1,
+                      );
+                    }
+                  ),
                   Consumer<User>(
                     builder: (_, userData,__) =>
                        ListView.separated(
