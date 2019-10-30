9806ae16fcd82567b5281f26c1dd6451fca19e16
unknown <ylasheen5@gmail.com>
2019-10-30 18:03:45 +0200

    add the plus card widget

---
+++ lib/widgets/add_task_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../bottomSheet.dart';
+
+class AddPageCard extends StatelessWidget {
+  final Color color;
+  final String userUID;
+
+  const AddPageCard({Key key, this.color = Colors.black, this.userUID}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 4.0,
+      margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(16.0),
+        color: Colors.white,
+        child: InkWell(
+          onTap: () {
+            Modal().mainBottomSheet(context, userUID);
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add,
+                  size: 52.0,
+                  color: color,
+                ),
+                Container(
+                  height: 8.0,
+                ),
+                Text(
+                  'Add Task',
+                  style: TextStyle(color: color),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
